a4807bbf8ec763f28d6a3617b0530c7d98f94cdd
= <phanthinhutranghahl@gmail.com>
2018-12-07 13:12:29 +0700

    Trang add button and textBox User Control BANVE

--- DoAnQLCB_Nhom10/View/ctrBanVe.Designer.cs
+++ DoAnQLCB_Nhom10/View/ctrBanVe.Designer.cs
@@ -28,18 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBanVe = new System.Windows.Forms.Button();
+            this.txtBoxMaCB = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtBoxMaKH = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxHangVe = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBoxCMND = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxSDT = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxGiaTien = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnHuy);
+            this.groupBox1.Controls.Add(this.txtBoxGiaTien);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtBoxSDT);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtBoxCMND);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtBoxHangVe);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBoxMaKH);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtBoxMaCB);
+            this.groupBox1.Controls.Add(this.btnBanVe);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(702, 545);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bán vé máy bay";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Chuyến Bay";
+            // 
+            // btnBanVe
+            // 
+            this.btnBanVe.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBanVe.Location = new System.Drawing.Point(525, 435);
+            this.btnBanVe.Name = "btnBanVe";
+            this.btnBanVe.Size = new System.Drawing.Size(135, 45);
+            this.btnBanVe.TabIndex = 1;
+            this.btnBanVe.Text = "Bán Vé";
+            this.btnBanVe.UseVisualStyleBackColor = true;
+            // 
+            // txtBoxMaCB
+            // 
+            this.txtBoxMaCB.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxMaCB.Location = new System.Drawing.Point(153, 65);
+            this.txtBoxMaCB.Name = "txtBoxMaCB";
+            this.txtBoxMaCB.Size = new System.Drawing.Size(139, 26);
+            this.txtBoxMaCB.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtBoxMaKH
+            // 
+            this.txtBoxMaKH.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxMaKH.Location = new System.Drawing.Point(474, 65);
+            this.txtBoxMaKH.Name = "txtBoxMaKH";
+            this.txtBoxMaKH.Size = new System.Drawing.Size(186, 26);
+            this.txtBoxMaKH.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(337, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mã Khách Hàng";
+            // 
+            // txtBoxHangVe
+            // 
+            this.txtBoxHangVe.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxHangVe.Location = new System.Drawing.Point(153, 191);
+            this.txtBoxHangVe.Name = "txtBoxHangVe";
+            this.txtBoxHangVe.Size = new System.Drawing.Size(139, 26);
+            this.txtBoxHangVe.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 18);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Hạng vé";
+            // 
+            // txtBoxCMND
+            // 
+            this.txtBoxCMND.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxCMND.Location = new System.Drawing.Point(153, 126);
+            this.txtBoxCMND.Name = "txtBoxCMND";
+            this.txtBoxCMND.ReadOnly = true;
+            this.txtBoxCMND.Size = new System.Drawing.Size(139, 26);
+            this.txtBoxCMND.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 129);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "CMND";
+            this.label4.Visible = false;
+            // 
+            // txtBoxSDT
+            // 
+            this.txtBoxSDT.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxSDT.Location = new System.Drawing.Point(474, 123);
+            this.txtBoxSDT.Name = "txtBoxSDT";
+            this.txtBoxSDT.ReadOnly = true;
+            this.txtBoxSDT.Size = new System.Drawing.Size(186, 26);
+            this.txtBoxSDT.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(337, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Số điện thoại";
+            // 
+            // txtBoxGiaTien
+            // 
+            this.txtBoxGiaTien.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxGiaTien.Location = new System.Drawing.Point(474, 196);
+            this.txtBoxGiaTien.Name = "txtBoxGiaTien";
+            this.txtBoxGiaTien.ReadOnly = true;
+            this.txtBoxGiaTien.Size = new System.Drawing.Size(186, 26);
+            this.txtBoxGiaTien.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(337, 199);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 18);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Giá tiền";
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(340, 435);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(135, 45);
+            this.btnHuy.TabIndex = 13;
+            this.btnHuy.Text = "Hủy";
+            this.btnHuy.UseVisualStyleBackColor = true;
             // 
             // ctrBanVe
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
             this.Name = "ctrBanVe";
             this.Size = new System.Drawing.Size(702, 547);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBoxHangVe;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBoxMaKH;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxMaCB;
+        private System.Windows.Forms.Button btnBanVe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtBoxSDT;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxCMND;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxGiaTien;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnHuy;
     }
 }
