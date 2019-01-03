54523603af456295f795a7a64765878347866dd8
= <phanthinhutranghahl@gmail.com>
2019-01-03 09:24:04 +0700

    finish my Class DTO - KhachHang,ChiTietChuyenBay,VeChuyenBay

---
+++ DoAnLTUD_QLCB/DTO/ChiTietChuyenBayDTO.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class ChiTietChuyenBayDTO
+    {
+        int _stt;
+        string _maCB;
+        string _maSBTG;
+        int _tgDung;
+        string _ghiChu;
+        public int STT
+        {
+            get
+            {
+                return _stt;
+            }
+
+            set
+            {
+                _stt = value;
+            }
+        }
+        public string MaCB
+        {
+            get
+            {
+                return _maCB;
+            }
+
+            set
+            {
+                _maCB = value;
+            }
+        }
+        public string MaSBTG
+        {
+            get
+            {
+                return _maSBTG;
+            }
+
+            set
+            {
+                _maSBTG = value;
+            }
+        }
+        public string GhiChu
+        {
+            get
+            {
+                return _ghiChu;
+            }
+
+            set
+            {
+                _ghiChu = value;
+            }
+        }
+        public int TGDung
+        {
+            get
+            {
+                return _tgDung;
+            }
+
+            set
+            {
+                _tgDung = value;
+            }
+        }
+    }
+}
