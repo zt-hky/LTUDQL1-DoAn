c3a2850068837e9068e1f37be3c312f0241f78e4
HQTruong245 <truonghq.hcmus@gmail.com>
2019-01-06 07:03:41 +0700

    Create BaoCaoBUS, BaoCaoDAO

---
+++ DoAnLTUD_QLCB/BUS/BaoCaoBUS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BUS
+{
+    public class BaoCaoBUS
+    {
+        private static BaoCaoBUS instance;
+
+        public static BaoCaoBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                    BaoCaoBUS.instance = new BaoCaoBUS();
+                return instance;
+            }
+        }
+
+        private BaoCaoBUS() { }
+
+    }
+}
---
+++ DoAnLTUD_QLCB/DAO/BaoCaoDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAO
+{
+    public class BaoCaoDAO
+    {
+        private static BaoCaoDAO instance;
+
+        public static BaoCaoDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                    BaoCaoDAO.instance = new BaoCaoDAO();
+                return instance;
+            }
+        }
+    }
+}
