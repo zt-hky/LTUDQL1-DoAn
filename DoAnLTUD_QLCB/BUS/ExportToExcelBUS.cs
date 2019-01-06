b91c91c66125cae8c0341335587f73ac7465999f
= <phanthinhutranghahl@gmail.com>
2019-01-06 20:10:16 +0700

    fix loi cho Truong

--- DoAnLTUD_QLCB/BUS/ExportToExcelBUS.cs
+++ DoAnLTUD_QLCB/BUS/ExportToExcelBUS.cs
@@ -26,12 +26,12 @@ namespace BUS
 
         public void ExportThang(DataTable dt, string sheetName = "Thống Kê Tháng", string title = "Báo Cáo Doanh Thu Bán Vé Các Chuyến Bay")
         {
-            //ExportToExcelDAO.Instance.ExportThang(dt, sheetName, title);
+            ExportToExcelDAO.Instance.ExportThang(dt, sheetName, title);
         }
 
         public void ExportNam(DataTable dt, string sheetName="Thống Kê Năm", string title= "Báo Cáo Doanh Thu Năm")
         {
-            //ExportToExcelDAO.Instance.ExportNam(dt, sheetName, title);
+            ExportToExcelDAO.Instance.ExportNam(dt, sheetName, title);
         }
 
     }
